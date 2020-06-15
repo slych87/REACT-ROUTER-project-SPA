f69ef4835bbebe7c571d812adfca8849aee7f9e0
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-15 12:05:34 +0200

    up date

--- SPA/src/App.js
+++ SPA/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Link, Route, NavLink } from 'react-router-dom'
 
+const Home = () => <h1>Strona startowa</h1>;
+const News = () => <h1>Aktualności</h1>;
+const Contact = () => <h1>Kontakt do nas</h1>;
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <Router>
         <div>
           <header>
             <nav>
@@ -15,17 +18,24 @@ class App extends Component {
                 <li><a href="/news">Aktualności</a></li>
                 <li><a href="/contact">Kontakt</a></li>*/}
 
-                <li><Link to="/">Start</Link></li>
-                <li><Link to="/news">Aktualności</Link></li>
-                <li><Link to="/contact">Kontakt</Link></li>
+                <li><NavLink to="/" exact activeClassName="home_selected"
+                  activeStyle={{
+                    backgroundColor: "black",
+                    letterSpacing: '2px',
+                  }}
+                >Start</NavLink></li>
+                <li><NavLink to="/news" activeClassName="news_selected">Aktualności</NavLink></li>
+                <li><NavLink to="/contact" activeClassName="contact_selected">Kontakt</NavLink></li>
               </ul>
             </nav>
           </header>
           <section>
-            Strona - witaj
-      </section>
+            <Route path="/" exact component={Home} />
+            <Route path="/news" component={News} />
+            <Route path="/contact" component={Contact} />
+          </section>
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
